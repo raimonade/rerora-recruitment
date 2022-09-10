f87e603262c5a6eb523eb68847fbc2ee60f67c18
Raimonds Korzenevskis <28185559+raimonade@users.noreply.github.com>
2022-09-11 00:53:04 +0300

    text color fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import AppHeader from '@/components/AppHeader';
 import Center from '@/components/Center';
 
 const Home = () => (
-  <div class="bg-black text-white w-full h-full flex flex-col justify-center items-center font-Inter tracking-tighter px-4 sm:px-8 lg:px-16 xl:px-20 w-screen min-h-screen relative text-stone-900 py-8 ">
+  <div class="bg-black text-white w-full h-full flex flex-col justify-center items-center font-Inter tracking-tighter px-4 sm:px-8 lg:px-16 xl:px-20 w-screen min-h-screen relative  py-8 ">
     <AppHeader />
     <Center />
     <div class="mt-auto w-full">
