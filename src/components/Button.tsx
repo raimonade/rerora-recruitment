9635847dfc5a2114165a9b2ab489bf88bea30400
Raimonds Korzenevskis <28185559+raimonade@users.noreply.github.com>
2022-09-11 03:02:52 +0300

    mailto fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 const Button = () => {
   return (
-    <a href="roberts@rerora-recruitment.com" class="mt-10 px-10 py-4 uppercase  font-semibold text-sm bg-white text-black rounded-full shadow-sm">
+    <a href="mailto:roberts@rerora-recruitment.com" class="mt-10 px-10 py-4 uppercase  font-semibold text-sm bg-white text-black rounded-full shadow-sm">
         Contact Us
     </a>
   );
