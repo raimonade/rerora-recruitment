afd729a5c5027a32f4d5e9e21bc2c2cdfbd10884
raimonade <Romka995@gmail.com>
2022-09-13 05:25:06 +0300

    text size

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -8,13 +8,13 @@ const Center = () => {
   return (
     <div class="my-auto w-full">
       <Container className="min-h-64 md:pb-28 pb-20 relative">
-        <h1 class="flex flex-col md:pb-8 pb-4 md:mt-auto self-center font-extrabold items-start text-7xl md:text-9xl xl:text-10xl bold tracking-tight">
+        <h1 class="flex flex-col md:pb-8 pb-4 md:mt-auto self-center font-extrabold items-start text-5xl md:text-7xl xl:text-9xl bold tracking-tight">
           <AppearIn delay={0.03}>Recruitment </AppearIn>
           <AppearIn delay={0.03}>made easy </AppearIn>
         </h1>
         <div class="md:mb-8 mb-5">
           <AppearIn delay={0.06}>
-            <p class="max-w-l sm:max-w-xl lg:max-w-2xl text-lg md:text-xl xl:text-2xl font-normal text-neutral-300">
+            <p class="max-w-l sm:max-w-xl lg:max-w-2xl text-m md:text-l xl:text-xl font-normal text-neutral-300">
               Hands-on approach to recruitment tailored to the online entertainment and gaming
               industry
             </p>
