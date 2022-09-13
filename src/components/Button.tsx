076526d8870349cdff3c06e7b1b3b3734de92e33
raimonade <Romka995@gmail.com>
2022-09-13 05:21:05 +0300

    padding and color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,10 @@
 const Button = () => {
   return (
-    // <a href="mailto:roberts@rerora-recruitment.com" class=" focus:outline-none focus:ring-blue-300 focus:ring-4  uppercase font-semibold text-sm bg-white text-black rounded-full shadow-sm mt-14 px-8 py-4 md:px-14 md:py-6">
-    <a href="mailto:roberts@rerora-recruitment.com" class="inline-flex  text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100  font-medium rounded-full text-sm md:text-m px-5 py-2.5 mr-2 md:px-6 md:py-3">
-        Contact Us
+    <a
+      href="mailto:roberts@rerora-recruitment.com"
+      class="inline-flex  text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100  font-medium rounded-full tlg:max-w-2xl text-lg md:text-xl xl:text-2xl px-5 py-2.5 mr-2 md:px-6 md:py-3"
+    >
+      Contact Us
     </a>
   );
 };
--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -2,18 +2,19 @@ import Button from './Button';
 import Container from './Container';
 import AppearIn from './AppearIn';
 
+//<h1 class=" mb-12"
+//<p class="text-lg" style="color: hsl(218, 81%, 95%)">
 const Center = () => {
   return (
     <div class="my-auto w-full">
-      <Container className="min-h-64 pb-28 relative">
-        <h1 class="flex flex-col pb-8 md:mt-auto self-center font-bold lg:text-9xl md:text-8xl text-5xl sm:text-8xl items-start">
-          {/* <AppearIn><span >Recruitment</span> </AppearIn> */}
+      <Container className="min-h-64 md:pb-28 pb-20 relative">
+        <h1 class="flex flex-col md:pb-8 pb-4 md:mt-auto self-center font-extrabold items-start text-7xl md:text-9xl xl:text-10xl bold tracking-tight">
           <AppearIn delay={0.03}>Recruitment </AppearIn>
           <AppearIn delay={0.03}>made easy </AppearIn>
         </h1>
-        <div class="mb-10">
+        <div class="md:mb-8 mb-5">
           <AppearIn delay={0.06}>
-            <p class="text-l sm:text-xl max-w-l lg:text-2xl sm:max-w-xl lg:max-w-2xl text-neutral-400">
+            <p class="max-w-l sm:max-w-xl lg:max-w-2xl text-lg md:text-xl xl:text-2xl font-normal text-neutral-300">
               Hands-on approach to recruitment tailored to the online entertainment and gaming
               industry
             </p>
